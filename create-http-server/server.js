84205208ede6a6507a31ab75be143667befe27c8
soheekimdev <soheekim.dev@gmail.com>
2024-11-14 15:10:21 +0900

    Feat: 서버에서 preflight 요청 처리, Todo List에 더미데이터 추가

--- create-http-server/server.js
+++ create-http-server/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-let todo = [];
+let todo = [{ id: 1, content: '더미데이터' }];
 
 // 서버 만들기
 const server = http.createServer((req, res) => {
@@ -10,7 +10,16 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  res.end('end!');
+  // preflight 요청 처리
+  if (req.method === 'OPTIONS') {
+    return res.end('요청 보내세요.');
+  }
+
+  if (req.method === 'GET') {
+    return res.end(JSON.stringify(todo));
+  }
+
+  return res.end('end!');
 });
 
 // 서버 실행
--- create-http-server/todo.js
+++ create-http-server/todo.js
@@ -1,7 +1,19 @@
 'use strict';
 
-const createButton = document.querySelector('button');
+const todoInput = document.querySelector('#todo-input input');
+const createButton = document.querySelector('#todo-input button');
+const ul = document.querySelector('#todo-list');
+
+fetch('http://localhost:3000')
+  .then((res) => res.json())
+  .then((res) => {
+    const list = document.createElement('li');
+    list.textContent = res[0].content;
+    ul.append(list);
+  });
 
 createButton.addEventListener('click', () => {
-  fetch('http://localhost:3000').then((res) => console.log(res));
+  fetch('http://localhost:3000')
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 });
