40719d32c6dac6aae5915609dc0f001921a2d13e
soheekimdev <soheekim.dev@gmail.com>
2024-11-14 22:57:19 +0900

    Refactor: fetch를 axios로 대체

--- create-express-server/todo.js
+++ create-express-server/todo.js
@@ -6,45 +6,36 @@ const todoInput = document.querySelector('#todo-input input');
 const createButton = document.querySelector('#todo-input button');
 const ul = document.querySelector('#todo-list');
 
+const initInput = () => {
+  todoInput.value = '';
+};
+
+// CRUD
 const createTodo = () => {
   const newTodo = todoInput.value;
+  initInput();
 
-  return fetch(BASE_URL, {
-    method: 'POST',
-    body: newTodo,
-  })
-    .then((res) => res.text())
-    .then((res) => console.log(res));
+  return axios
+    .post(BASE_URL, newTodo, {
+      headers: { 'Content-Type': 'text/plain' },
+    })
+    .then((res) => console.log(res.data));
 };
 
 const readTodo = async () => {
-  const res = await fetch(BASE_URL);
-  const data = await res.json();
-
-  return data;
+  const res = await axios.get(BASE_URL);
+  return res.data;
 };
 
 const updateTodo = (newTodo) => {
-  return fetch(BASE_URL, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newTodo),
-  })
-    .then((res) => res.text())
-    .then((res) => console.log(res));
+  return axios.put(BASE_URL, newTodo).then((res) => console.log(res.data));
 };
 
 const deleteTodo = (id) => {
-  return fetch(BASE_URL, {
-    method: 'DELETE',
-    body: id,
-  })
-    .then((res) => res.text())
-    .then((res) => console.log(res));
+  return axios.delete(BASE_URL, { data: id }).then((res) => console.log(res.data));
 };
 
+// UI 관련 로직
 const renderDisplay = (data) => {
   for (let el of data) {
     const list = document.createElement('li');
@@ -55,6 +46,10 @@ const renderDisplay = (data) => {
 
     updateButton.textContent = '수정';
     updateButton.onclick = () => {
+      if (updateInput.value.trim() === '') {
+        alert('내용을 입력하세요.');
+        return;
+      }
       updateTodo({
         id: el.id,
         content: updateInput.value,
@@ -89,7 +84,12 @@ const removeDisplay = () => {
   }
 };
 
+// 이벤트 등록
 createButton.addEventListener('click', () => {
+  if (todoInput.value.trim() === '') {
+    alert('할 일을 입력하세요.');
+    return;
+  }
   createTodo()
     .then(() => readTodo())
     .then((res) => {
