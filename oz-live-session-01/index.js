b3ee0bf2243b5a00f2419175187109989dd2d3dc
soheekimdev <soheekim.dev@gmail.com>
2024-11-14 18:28:25 +0900

    Feat: 경로에 id값을 받아서 사용하는 방식 실습

--- oz-live-session-01/index.js
+++ oz-live-session-01/index.js
@@ -1,12 +1,14 @@
-console.log('Hello World');
-
 const express = require('express');
 const app = express();
 
 app.use(express.json()); // JSON 요청을 받기 위한 설정
 
-app.get('/', (req, res) => { // get 대신 post, put, delete 등을 사용할 수 있음. (HTTP method 지정)
-  res.send('Hello, Express!');
+app.get('/:id', (req, res) => { // get 대신 post, put, delete 등을 사용할 수 있음. (HTTP method 지정)
+  const params = req.params;
+  console.log(params);
+  console.log('id', params.id);
+
+  res.send('Hello, Node.js!');
 });
 
 app.listen(1004, () => { // 원하는 포트 입력
