4715636d3c1d1bd2e8b4b2c1c3103688f9d3ebab
soheekimdev <soheekim.dev@gmail.com>
2024-11-14 16:09:51 +0900

    Feat: Todo List CRUD 기능 구현

--- create-http-server/server.js
+++ create-http-server/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 
-let todo = [{ id: 1, content: '더미데이터' }];
+let todos = [
+  { id: 1, content: '더미데이터' },
+  { id: 2, content: '터미네이터' },
+];
 
 // 서버 만들기
 const server = http.createServer((req, res) => {
@@ -8,7 +11,7 @@ const server = http.createServer((req, res) => {
 
   // CORS 설정
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
 
   // preflight 요청 처리
   if (req.method === 'OPTIONS') {
@@ -16,7 +19,49 @@ const server = http.createServer((req, res) => {
   }
 
   if (req.method === 'GET') {
-    return res.end(JSON.stringify(todo));
+    return res.end(JSON.stringify(todos));
+  }
+
+  if (req.method === 'POST') {
+    let data;
+    req.on('data', (chunk) => {
+      data = chunk.toString();
+    });
+    req.on('end', () => {
+      const newTodo = { id: Number(new Date()), content: data };
+      todos.push(newTodo);
+    });
+    return res.end('Todo가 추가됐습니다.');
+  }
+
+  if (req.method === 'PUT') {
+    let data;
+    req.on('data', (chunk) => {
+      data = chunk.toString();
+    });
+    req.on('end', () => {
+      const newTodo = JSON.parse(data);
+      todos = todos.map((el) => {
+        if (el.id === newTodo.id) {
+          return newTodo;
+        } else {
+          return el;
+        }
+      });
+    });
+    return res.end('Todo가 수정됐습니다.');
+  }
+
+  if (req.method === 'DELETE') {
+    let data;
+    req.on('data', (chunk) => {
+      data = chunk.toString();
+    });
+    req.on('end', () => {
+      const id = Number(data);
+      todos = todos.filter((el) => el.id !== id);
+    });
+    return res.end('Todo가 삭제됐습니다.');
   }
 
   return res.end('end!');
--- create-http-server/todo.js
+++ create-http-server/todo.js
@@ -1,19 +1,98 @@
 'use strict';
 
+const BASE_URL = 'http://localhost:3000';
+
 const todoInput = document.querySelector('#todo-input input');
 const createButton = document.querySelector('#todo-input button');
 const ul = document.querySelector('#todo-list');
 
-fetch('http://localhost:3000')
-  .then((res) => res.json())
-  .then((res) => {
+const createTodo = () => {
+  const newTodo = todoInput.value;
+
+  return fetch(BASE_URL, {
+    method: 'POST',
+    body: newTodo,
+  })
+    .then((res) => res.text())
+    .then((res) => console.log(res));
+};
+
+const readTodo = async () => {
+  const res = await fetch(BASE_URL);
+  const data = await res.json();
+
+  return data;
+};
+
+const updateTodo = (newTodo) => {
+  return fetch(BASE_URL, {
+    method: 'PUT',
+    body: JSON.stringify(newTodo),
+  })
+    .then((res) => res.text())
+    .then((res) => console.log(res));
+};
+
+const deleteTodo = (id) => {
+  return fetch(BASE_URL, {
+    method: 'DELETE',
+    body: id,
+  })
+    .then((res) => res.text())
+    .then((res) => console.log(res));
+};
+
+const renderDisplay = (data) => {
+  for (let el of data) {
     const list = document.createElement('li');
-    list.textContent = res[0].content;
+    list.textContent = el.content;
+
+    const updateInput = document.createElement('input');
+    const updateButton = document.createElement('button');
+
+    updateButton.textContent = '수정';
+    updateButton.onclick = () => {
+      updateTodo({
+        id: el.id,
+        content: updateInput.value,
+      })
+        .then(() => readTodo())
+        .then((res) => {
+          removeDisplay();
+          renderDisplay(res);
+        });
+    };
+
+    const deleteButton = document.createElement('button');
+
+    deleteButton.textContent = '삭제';
+    deleteButton.onclick = () => {
+      deleteTodo(el.id)
+        .then(() => readTodo())
+        .then((res) => {
+          removeDisplay();
+          renderDisplay(res);
+        });
+    };
+
+    list.append(updateInput, updateButton, deleteButton);
     ul.append(list);
-  });
+  }
+};
+
+const removeDisplay = () => {
+  while (ul.children.length) {
+    ul.removeChild(ul.children[0]);
+  }
+};
 
 createButton.addEventListener('click', () => {
-  fetch('http://localhost:3000')
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+  createTodo()
+    .then(() => readTodo())
+    .then((res) => {
+      removeDisplay();
+      renderDisplay(res);
+    });
 });
+
+readTodo().then((res) => renderDisplay(res));
